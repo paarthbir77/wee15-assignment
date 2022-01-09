db1afbaa4aa35e871912fb81e9166200f9e43178
paarthbir77 <42001307+paarthbir77@users.noreply.github.com>
2022-01-09 12:50:06 +0530

    assignment solution

---
+++ user-microservice/src/main/java/com/example/user/config/RestTemplateConfig.java
@@ -0,0 +1,52 @@
+package com.example.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+@Configuration
+public class RestTemplateConfig {
+	//@Bean
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
+	
+	@Bean
+    public RestTemplate restTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException,
+            CertificateException, MalformedURLException, IOException {
+ 
+		SSLContextBuilder sslcontext = new SSLContextBuilder();
+        sslcontext.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLContext(sslcontext.build()).setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .build();
+//        SSLContext sslContext = new SSLContextBuilder()
+//                .loadTrustMaterial(new URL("file:src/main/resources/security_certificate.p12"), "password".toCharArray()).build();
+//        SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext);
+// 
+//        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslConFactory).build();
+        
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+ 
+        return restTemplate;
+ 
+    }
+
+}
